9f1c2fe0118a9b4351fd07fbc127baa61d39a646
minji <ireneeming@gmail.com>
2024-05-23 15:47:02 +0900

    🚧 : 맛집 페이지 navbar 작업중

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import type { InferGetStaticPropsType, GetStaticProps } from "next";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+ return <Component {...pageProps} />;
 }
