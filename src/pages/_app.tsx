ee0cb1b5e81deacdfeb5273678d79b2b74fc6a0f
minji <ireneeming@gmail.com>
2024-05-24 00:15:54 +0900

    ✨ : kakaomap 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { InferGetStaticPropsType, GetStaticProps } from "next";
-import Layout from "@/component/Layout";
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
  return (
