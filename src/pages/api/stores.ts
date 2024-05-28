3bd864ce5f4a4053a32c1d051766c41a9277dd5f
minji <ireneeming@gmail.com>
2024-05-28 23:28:27 +0900

    ✨ : pagenation 구현하기

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -1,19 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { StoreType } from "@/interface";
+import { StoreApiResponse } from "@/interface";
 import { PrismaClient } from "../../../prisma";
 
 export default async function handler(
  req: NextApiRequest,
- res: NextApiResponse<StoreType[]>
+ res: NextApiResponse<StoreApiResponse>
 ) {
- //  const stores = (await import("../../data/store_data.json"))[
- //   "DATA"
- //  ] as StoreType[];
-
+ const { page = "1" }: { page?: string } = req.query; // request에서 page 값 가져오기 기본값=1
+ const skipPage = parseInt(page) - 1; // 기본 페이지는 1부터 시작하니까 처음부터 시작하기 위해 생성
  const prisma = new PrismaClient();
+
+ const count = await prisma.store.count(); //prisma에서 호출된 데이터가 몇개인지 count
  const stores = await prisma.store.findMany({
   orderBy: { id: "asc" },
+  take: 10, // 항상 데이터 10개만 가져오기
+  skip: skipPage * 10, // 해당 숫자 페이지를 가져옴
  });
 
- res.status(200).json(stores);
+ //totalpage, data, page, totalCount
+
+ res.status(200).json({
+  page: parseInt(page),
+  data: stores,
+  totalCount: count, // 총 레코드의 갯수
+  totalPage: Math.ceil(count / 10), // 총 레코드 갯수 /10으로 총 몇페이지 나왔는지 계산
+ });
 }
--- src/pages/stores/index.tsx
+++ src/pages/stores/index.tsx
@@ -1,18 +1,24 @@
-import { StoreType } from "@/interface/index";
+import { StoreApiResponse, StoreType } from "@/interface/index";
 import Image from "next/image";
 import axios from "axios";
 import { useQuery } from "react-query";
 import Loading from "@/components/Loading";
+import { useRouter } from "next/router";
+
+import Link from "next/link";
 
 export default function StoreListPage() {
+ const router = useRouter();
+ const { page = "1" } = router.query;
+
  const {
   isLoading,
   isError,
   data: stores,
- } = useQuery("stores", async () => {
-  const { data } = await axios("/api/stores");
+ } = useQuery(`stores-${page}`, async () => {
+  const { data } = await axios(`/api/stores?page=${page}`);
 
-  return data as StoreType[];
+  return data as StoreApiResponse;
  });
 
  if (isError) {
@@ -25,7 +31,7 @@ export default function StoreListPage() {
     {isLoading ? (
      <Loading />
     ) : (
-     stores?.map((store, idx) => (
+     stores?.data.map((store, idx) => (
       <li className="flex justify-between gap-x-6 py-5" key={idx}>
        <div className="flex gap-x-4">
         <Image
@@ -60,14 +66,54 @@ export default function StoreListPage() {
      ))
     )}
    </ul>
+   {stores?.totalPage && (
+    <div className="py-6 w-full px-10 flex justify-center gap-4 bg-white my-10 flex-wrap text-black">
+     {stores?.totalPage <= 10 ? (
+      [...Array(stores?.totalPage)].map((x, i) => (
+       <Link href={{ pathname: "/stores", query: { page: i + 1 } }} key={i}>
+        <span
+         className={`px-3 py-2 rounded border shadow-sm bg-white ${
+          i + 1 === parseInt(page, 10)
+           ? "text-blue-600 font-bold"
+           : "text-gray-300"
+         }`}
+        >
+         {i + 1}
+        </span>
+       </Link>
+      ))
+     ) : (
+      <>
+       {parseInt(page) > 1 && (
+        <Link
+         href={{ pathname: "/stores", query: { page: parseInt(page) - 1 } }}
+        >
+         <span className={`px-3 py-2 rounded border shadow-sm bg-white`}>
+          이전
+         </span>
+        </Link>
+       )}
+
+       <Link href={{ pathname: "/stores", query: { page: page } }}>
+        <span
+         className={`px-3 py-2 rounded border shadow-sm bg-white text-blue-600`}
+        >
+         {page}
+        </span>
+       </Link>
+       {stores?.totalPage > parseInt(page) && (
+        <Link
+         href={{ pathname: "/stores", query: { page: parseInt(page) + 1 } }}
+        >
+         <span className={`px-3 py-2 rounded border shadow-sm bg-white`}>
+          다음
+         </span>
+        </Link>
+       )}
+      </>
+     )}
+    </div>
+   )}
   </div>
  );
 }
-
-// export async function getServerSideProps() {
-//  const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
-
-//  return {
-//   props: { stores: stores.data },
-//  };
-// }
