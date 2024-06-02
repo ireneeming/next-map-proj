b75db47653b2d84a4b53f6109abe9d01dea2e99a
minji <ireneeming@gmail.com>
2024-06-02 17:05:08 +0900

    🐛 :navbar nextauth session, callback 이슈 처리

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
 
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-
-import { useSession, signOut } from "next-auth/react";
 
 export default function Navbar() {
  const [isOpen, setIsOpen] = useState(false);
  const { data, status } = useSession();
 
- console.log(data, status);
  return (
   <>
    <div className="navbar">
-    <div className="navbar__logo">nextmap</div>
+    <Link className="navbar__logo" href="/">
+     nextmap
+    </Link>
     <div className="navbar__list">
      <Link href="/stores" className="navbar__list--item">
       맛집 목록
@@ -25,7 +25,9 @@ export default function Navbar() {
      <Link href="/users/likes" className="navbar__list--item">
       찜한 가게
      </Link>
-
+     <Link href="/users/mypage" className="navbar__list--item">
+      마이페이지
+     </Link>
      {status === "authenticated" ? (
       <button type="button" onClick={() => signOut()}>
        로그아웃
@@ -36,36 +38,64 @@ export default function Navbar() {
       </Link>
      )}
     </div>
-
+    {/* mobile button */}
     <div
-     role={"presentation"}
-     onClick={() => {
-      setIsOpen((val) => !val);
-     }}
+     role="presentation"
      className="navbar__button"
+     onClick={() => setIsOpen((val) => !val)}
     >
      {isOpen ? <AiOutlineClose /> : <BiMenu />}
     </div>
    </div>
-
+   {/* mobile navbar */}
    {isOpen && (
     <div className="navbar--mobile">
      <div className="navbar__list--mobile">
-      <Link href="/stores" className="navbar__list--item--mobile">
+      <Link
+       href="/stores"
+       className="navbar__list--item--mobile"
+       onClick={() => setIsOpen(false)}
+      >
        맛집 목록
       </Link>
-      <Link href="/stores/new" className="navbar__list--item--mobile">
+      <Link
+       href="/stores/new"
+       className="navbar__list--item--mobile"
+       onClick={() => setIsOpen(false)}
+      >
        맛집 등록
       </Link>
-      <Link href="/users/likes" className="navbar__list--item--mobile">
+      <Link
+       href="/users/likes"
+       className="navbar__list--item--mobile"
+       onClick={() => setIsOpen(false)}
+      >
        찜한 가게
       </Link>
+      <Link
+       href="/users/mypage"
+       className="navbar__list--item--mobile"
+       onClick={() => setIsOpen(false)}
+      >
+       마이페이지
+      </Link>
       {status === "authenticated" ? (
-       <button type="button" onClick={() => signOut()}>
+       <button
+        type="button"
+        onClick={() => {
+         signOut();
+         setIsOpen(false);
+        }}
+        className="navbar__list--item--mobile text-left"
+       >
         로그아웃
        </button>
       ) : (
-       <Link href="/api/auth/signin" className="navbar__list--item--mobile">
+       <Link
+        href="/api/auth/signin"
+        className="navbar__list--item--mobile"
+        onClick={() => setIsOpen(false)}
+       >
         로그인
        </Link>
       )}
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,15 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/db";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
  // Configure one or more authentication providers
+ session: {
+  strategy: "jwt" as const,
+  maxAge: 60 * 60 * 24,
+  updateAge: 60 * 60 * 2,
+ },
  adapter: PrismaAdapter(prisma),
  providers: [
   // ...add more providers here
@@ -15,6 +20,21 @@ export const authOptions = {
  ],
  pages: {
   signIn: "/users/login",
+ },
+ callbacks: {
+  session: ({ session, token }) => ({
+   ...session,
+   user: {
+    ...session.user,
+    id: token.sub,
+   },
+  }),
+  jwt: async ({ user, token }) => {
+   if (user) {
+    token.sub = user.id;
+   }
+   return token;
+  },
  },
 };
 
