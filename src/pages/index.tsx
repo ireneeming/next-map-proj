702604690ce433cc81847cc6b62970a27fa05c1d
minji <ireneeming@gmail.com>
2024-06-02 18:17:24 +0900

    🐛 : fix build error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,11 +29,19 @@ export default function Home({ stores }: { stores: StoreType[] }) {
 //  };
 // }
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//  const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
+
+//  return {
+//   props: { stores: stores.data },
+//   revalidate: 60 * 60,
+//  };
+// }
+
+export async function getServerSideProps() {
  const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
 
  return {
   props: { stores: stores.data },
-  revalidate: 60 * 60,
  };
 }
